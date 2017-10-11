f856ddc2b194cfcd1b7f32a1cce7b9cc29023cf2
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-10-11 11:52:53 +0200

    forgot to add files

---
+++ view/indexv.php
@@ -0,0 +1,11 @@
+<?php require 'template/head.php'; ?>
+
+<form class="" action="" method="post">
+    <p>
+      <label for="name">Nom : <input id="name" type="text" name="nom" maxlength="50" /></label>
+      <input type="submit" value="Créer ce personnage" name="Creer" class="btn"/>
+      <input type="submit" value="Utiliser ce personnage" name="Utiliser" class="btn"/>
+    </p>
+</form>
+
+<?php require 'template/foot.php'; ?>
---
+++ view/template/foot.php
@@ -0,0 +1,10 @@
+<!-- ############## JS sources ############## -->
+  <script src="js/plugins.js"></script>
+  <script src="js/jquery-3.2.1.js"></script>
+  <script src="js/tether.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/main.js"></script>
+<!-- ############## JS sources ############## -->
+</body>
+
+</html>
